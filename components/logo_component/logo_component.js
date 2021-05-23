3c2dcff5ed2e66c4c2c59236a724293ae9b43506
vieradaniel <dariennx@hotmail.com>
2021-05-23 08:19:26 -0300

    testing an event listener

--- components/logo_component/logo_component.js
+++ components/logo_component/logo_component.js
@@ -1,9 +1,12 @@
-const pokemonLogo =`
-<div  class ="pokemon-logo-container">
+const template =document.createElement('template');
+template.innerHTML =
+`
+<div class ="pokemon-logo-container">
     <img src="static/pokemon-logo.png" class ="pokemon-logo" alt="">
 </div>
 
-<h2>LAST STANDING</h2>
+<h2 class="test"></h2>
+<button id="change-background">do something</button>
 
 `;
 
@@ -13,16 +16,31 @@ class LogoComponent extends HTMLElement{
 
     constructor(){
         super();
-   
-    this.innerHTML=`${ this.getAttribute('pokename')}`;// agarras el atributo del html que tiene el nombre pokename y pones el valor de ese key dentro del contenido
-    this.shadowRoot.innerHTML= pokemonLogo;
+    
+    this.attachShadow({ mode: 'open'});
+    //this.innerHTML=`${ this.getAttribute('pokename')}`;// agarras el atributo del html que tiene el nombre pokename y pones el valor de ese key dentro del contenido
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    
+    this.shadowRoot.querySelector('.test').innerText = this.getAttribute('test');
+
+    //this.shadowRoot.innerHTML= pokemonLogo;
         
     }
 
-    connectedCallBack(){
-        this.attachShadow({ mode: 'open'});
-        this.shadowRoot.innerHTML(pokemonLogo);
+    toggleBackground(){
+        console.log("123");
     }
+
+    connectedCallback(){
+       this.shadowRoot.querySelector('#change-background').addEventListener('click',()=>
+           this.toggleBackground());
+       
+    }
+
+    disconnectedCallback(){
+        this.shadowRoot.querySelector('#change-background').removeEventListener();
+    }
+    
 }
 
 window.customElements.define ('pokemon-logo', LogoComponent);
